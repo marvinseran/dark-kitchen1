0985e1d0fa60c104681913c87aceba62f45209b9
marvinseran <marvinseran@gmail.com>
2023-12-19 14:49:54 +0100

    restructurationgit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const collection = [
         name: "Pizza 4 Fromages",
         img: "img/pqf.jpg",
         genre: ["Pizza"],
+        desc: "La pizza ai quattro formaggi est garnie d'une combinaison de quatre sortes de fromages, généralement fondus ensemble, avec ou sans sauce tomate.",
     },
     {
         priceShow : "12 €",
@@ -41,6 +42,7 @@ const collection = [
     {
         priceShow : "14 €",
         price: 14,
+        name: "Lasagne Bolognaise",
         img: "img/lsbol.jpg",
         genre: ["Spécialité de la maison"],
         desc: "Fines feuilles de pâtes avec une sauce bolognaise à base de viande hachée, de tomates, d'oignons, d'ail, de béchamel et de fromage.",
@@ -56,6 +58,7 @@ const collection = [
     {
         priceShow : "14 €",
         price: 14,
+        name: "Canneloni Bolognaise",
         img: "img/canbol.jpg",
         genre: ["Spécialité de la maison"],
         desc: "Des tubes de pâtes remplis d'une délicieuse farce à la viande, souvent préparée avec une sauce bolognaise, du fromage ricotta ou béchamel.",
@@ -119,6 +122,7 @@ collection.forEach(function(object, index) {
 
     createCard(section, 'div', 'card__desc', object.desc)
 
+    createCard(section, 'button', 'card__button', "Ajouter à la commande")
 
 
     
@@ -132,11 +136,7 @@ collection.forEach(function(object, index) {
     }
 });
 
-
-// ------------------PANIER---------------------------------
-
 function togglePopup() {
     let popup = document.querySelector(".popup");
     popup.classList.toggle("open");
-
 }
